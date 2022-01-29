0b6bc7d8aba7d9a27965af3115b5a3b79002de9f
Shankar Mata <mshankarrao1970@gmail.com>
2022-01-29 14:01:56 -0500

    near wallet fix

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -21,53 +21,32 @@ export class AppHeaderComponent implements OnInit {
   constructor(private dateService: DateService) {
   }
 
+ 
+  
+
+  // connect to NEAR
+  // const near = await connect(config);
+
+  signIn = async () => {
+    this.config = {
+      networkId: "testnet",
+      keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+      nodeUrl: "https://rpc.testnet.near.org",
+      walletUrl: "https://wallet.testnet.near.org",
+      helperUrl: "https://helper.testnet.near.org",
+      explorerUrl: "https://explorer.testnet.near.org",
+      headers: {}
+    };
+    const near = await connect(this.config);
+    var wallet = new WalletConnection(near,null);
+    wallet.requestSignIn(
+    );
+  };
+
   ngOnInit(): void {
-
-
-    // configure minimal network settings and key storage
-const config = {
-  nodeUrl: "https://rpc.testnet.near.org",
-  deps: {
-    keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore(),
-  },
-};
-
-
     this.todayDay = this.dateService.getTodayDay();
     this.todayDate = this.dateService.getTodayDate();
     (async function ($) {
-      const config = {
-        networkId: "testnet",
-        keyStore: new keyStores.BrowserLocalStorageKeyStore(),
-        nodeUrl: "https://rpc.testnet.near.org",
-        walletUrl: "https://wallet.testnet.near.org",
-        helperUrl: "https://helper.testnet.near.org",
-        explorerUrl: "https://explorer.testnet.near.org",
-        headers: {}
-      };
-      
-      this.config = config;
-      // connect to NEAR
-      const near = await connect(config);
-      
-      // create wallet connection
-      //const wallet = new WalletConnection(near);
-      
-      const signIn = async () => {
-        const near = await connect(this.config);
-        var wallet = new WalletConnection(near,null);
-        wallet.requestSignIn(
-          "example-contract.testnet", // contract requesting access
-          "Example App", // optional
-          "http://YOUR-URL.com/success", // optional
-          "http://YOUR-URL.com/failure" // optional
-        );
-      };
-
-      $(document).ready(
-        signIn()
-        )
-      
     })(jQuery);
   }
 
